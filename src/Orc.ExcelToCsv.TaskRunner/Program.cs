6fe8de7735cb49e5e1dacc3e218a9c2108bc7638
Samuel Hart <shart@penlink.com>
2016-01-12 23:16:58 -0600

    Add Orc.ExcelToCsv.TaskRunner Project to solution

---
+++ src/Orc.ExcelToCsv.TaskRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orc.ExcelToCsv.TaskRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
